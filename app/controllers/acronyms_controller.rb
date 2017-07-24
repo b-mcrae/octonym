5d64832bd41d1b85f2914635ae4fc7d41854832b
Brian McRae <brian.g.mcrae@gmail.com>
2017-07-23 19:49:52 -0700

    Add Update and Destroy methods. Add acronym validation.

--- app/controllers/acronyms_controller.rb
+++ app/controllers/acronyms_controller.rb
@@ -8,13 +8,38 @@ class AcronymsController < ApplicationController
   end
 
   def new
+    @acronym = Acronym.new
+  end
+
+  def edit
+    @acronym = Acronym.find(params[:id])
   end
 
   def create
     @acronym = Acronym.new(acronym_params)
 
-    @acronym.save
-    redirect_to @acronym
+    if @acronym.save
+      redirect_to @acronym
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @acronym = Acronym.find(params[:id])
+
+    if @acronym.update(acronym_params)
+      redirect_to @acronym
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @acronym = Acronym.find(params[:id])
+    @acronym.destroy
+
+    redirect_to acronyms_path
   end
 
   private
--- app/models/acronym.rb
+++ app/models/acronym.rb
@@ -1,2 +1,3 @@
 class Acronym < ApplicationRecord
+  validates :name, presence: true
 end
